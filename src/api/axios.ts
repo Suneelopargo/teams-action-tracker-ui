2c7660be298956d3a7ab9ce3bf5c61145e51365b
SuneelOpargo <suneel.kumar@solventek.com>
2026-07-01 20:34:36 +0530

    changed the base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,42 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-
-  baseURL:
-    'http://localhost:3000/api',
-
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-
-    'Content-Type':
-      'application/json',
-
+    'Content-Type': 'application/json',
   },
-
 });
 
-api.interceptors.request.use(
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
 
-  (config) => {
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    const token =
-      localStorage.getItem(
-        'accessToken',
-      );
-
-    if (
-      token &&
-      config.headers
-    ) {
-
-      config.headers.Authorization =
-        `Bearer ${token}`;
-
-    }
-
-    return config;
-
-  },
-
-);
+  return config;
+});
 
 export default api;
